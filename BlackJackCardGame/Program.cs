1c72bd222d16774d40bbff1336c0e04097cedd1e
1SaiVaruna <sai030804@gmail.com>
2024-11-02 12:20:19 +0000

    Added game loop and code to display scores for multiple rounds.

--- BlackJackCardGame/Program.cs
+++ BlackJackCardGame/Program.cs
@@ -9,6 +9,23 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("WELCOME TO BLACKJACK");
+
+			bool playAgain = true;
+
+			while (playAgain)
+			{
+				PlayRound();
+				Console.WriteLine("\nCURRENT SCORES: PLAYER: {0}, DEALER: {1}",playerScore, dealerScore);
+				Console.WriteLine("WOULD YOU LIKE TO PLAY AGAIN? (Y/N)");
+				playAgain = Console.ReadLine().ToUpper() == "Y";
+			}
+
+			Console.WriteLine("THANKS FOR PLAYING! FINAL SCORES: PLAYER: {0}, DEALER: {1}", playerScore, dealerScore);
+		}
+
+		static void PlayRound()
+		{
+
 		}
 	}
 }
