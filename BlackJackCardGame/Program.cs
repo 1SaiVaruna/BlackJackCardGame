086d38bb861c15e9b9ddd7db49d66263fa3ca046
1SaiVaruna <sai030804@gmail.com>
2024-11-02 12:15:56 +0000

    Created two variables for player and dealer scores.

--- BlackJackCardGame/Program.cs
+++ BlackJackCardGame/Program.cs
@@ -2,6 +2,10 @@
 {
 	internal class Program
 	{
+
+		private static int playerScore = 0;
+		private static int dealerScore = 0;
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("WELCOME TO BLACKJACK");
